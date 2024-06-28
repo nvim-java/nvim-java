3f7911de1538455a69f05dc22d3b6ebbfb1d4bc7
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-06-28 08:37:23 -0300

    feat: add lazy.lua file to declare dependencies on lazy.nvim

---
+++ lazy.lua
@@ -0,0 +1,22 @@
+return {
+	'nvim-java/nvim-java',
+	dependencies = {
+		'nvim-java/lua-async-await',
+		'nvim-java/nvim-java-refactor',
+		'nvim-java/nvim-java-core',
+		'nvim-java/nvim-java-test',
+		'nvim-java/nvim-java-dap',
+		'MunifTanjim/nui.nvim',
+		'neovim/nvim-lspconfig',
+		'mfussenegger/nvim-dap',
+		{
+			'williamboman/mason.nvim',
+			opts = {
+				registries = {
+					'github:nvim-java/mason-registry',
+					'github:mason-org/mason-registry',
+				},
+			},
+		},
+	},
+}
