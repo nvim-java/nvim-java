f92cadf7bdd01f5cfe383c8e725a283ffcc3685d
s1n7ax <srineshnisala@gmail.com>
2025-05-18 00:35:02 +0530

    Revert "🐛fix(`mason v2.0`): Compatibility added (#376)"
    This reverts commit d25bc1c55b4cea53f6174b2e2171ed8519113bc5.

--- lua/java/utils/mason.lua
+++ lua/java/utils/mason.lua
@@ -6,33 +6,42 @@ local await = async.wait_handle_ok
 local M = {}
 
 function M.is_available(package_name, package_version)
-	-- guard clause
 	local has_pkg = mason_reg.has_package(package_name)
+
 	if not has_pkg then
 		return false
 	end
 
-	-- check
+	local has_version = false
+
 	local pkg = mason_reg.get_package(package_name)
-	local version = pkg:get_installed_version()
-	local has_version = version == package_version
+	pkg:get_installed_version(function(success, version)
+		if success and version == package_version then
+			has_version = true
+		end
+	end)
 
 	return has_version
 end
 
 function M.is_installed(package_name, package_version)
-	-- guard clause
 	local pkg = mason_reg.get_package(package_name)
 	local is_installed = pkg:is_installed()
+
 	if not is_installed then
 		return false
 	end
 
-	-- check
-	local installed_version = pkg:get_installed_version()
-	is_installed = installed_version == package_version
+	local installed_version
+	pkg:get_installed_version(function(ok, version)
+		if not ok then
+			return
+		end
 
-	return is_installed
+		installed_version = version
+	end)
+
+	return installed_version == package_version
 end
 
 function M.is_outdated(packages)
