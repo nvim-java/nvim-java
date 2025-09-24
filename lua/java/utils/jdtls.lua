05a1e0d6927673b36e5effa4a00b28319a4b605f
Christopher Speck <cspeckrun@gmail.com>
2025-09-23 21:39:27 -0400

    maint: remove usages of lspconfig that are deprecated

--- lua/java/utils/jdtls.lua
+++ lua/java/utils/jdtls.lua
@@ -3,7 +3,7 @@ local get_error_handler = require('java.handlers.error')
 ---Returns an active jdtls client
 ---@return { client: LspClient }
 local function get_jdtls()
-	local clients = vim.lsp.get_active_clients({ name = 'jdtls' })
+	local clients = vim.lsp.get_clients({ name = 'jdtls' })
 
 	if #clients == 0 then
 		get_error_handler('could not find an active jdtls client')()
