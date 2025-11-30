452e440c84c15f3b3aeae82a5d9c956b72773331
s1n7ax <srineshnisala@gmail.com>
2025-11-30 18:45:57 +0530

    feat: add jproperties filetype support
    - Extract filetype config to separate module
    - Add jproperties to supported filetypes

---
+++ lua/java-core/ls/servers/jdtls/filetype.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+function M.get_filetypes()
+	return {
+		'java',
+		'jproperties',
+	}
+end
+
+return M
--- lua/java-core/ls/servers/jdtls/init.lua
+++ lua/java-core/ls/servers/jdtls/init.lua
@@ -8,6 +8,7 @@ function M.get_config(opts)
 	local cmd = require('java-core.ls.servers.jdtls.cmd')
 	local env = require('java-core.ls.servers.jdtls.env')
 	local root = require('java-core.ls.servers.jdtls.root')
+	local filetype = require('java-core.ls.servers.jdtls.filetype')
 	local log = require('java-core.utils.log2')
 
 	log.debug('get_config called with opts:', opts)
@@ -17,8 +18,8 @@ function M.get_config(opts)
 	base_conf.cmd = cmd.get_cmd(opts)
 	base_conf.cmd_env = env.get_env(opts)
 	base_conf.init_options.bundles = plugins.get_plugins(opts)
-	base_conf.filetypes = { 'java' }
 	base_conf.root_markers = root.get_root_markers()
+	base_conf.filetypes = filetype.get_filetypes()
 
 	return base_conf
 end
