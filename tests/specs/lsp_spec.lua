03d0d036c7761fc8de0c5a78adeff228aa8a9545
s1n7ax <srineshnisala@gmail.com>
2025-12-04 08:37:45 +0530

    chore: remove unwanted log

--- tests/specs/lsp_spec.lua
+++ tests/specs/lsp_spec.lua
@@ -1,6 +1,5 @@
 local lsp_utils = dofile('tests/utils/lsp-utils.lua')
 local assert = require('luassert')
-local system = require('java-core.utils.system')
 
 describe('LSP Attach', function()
 	it('should attach when opening a Java buffer', function()
@@ -9,9 +8,6 @@ describe('LSP Attach', function()
 		local jdtls = lsp_utils.wait_for_lsp_attach('jdtls', 30000)
 		local spring = lsp_utils.wait_for_lsp_attach('spring-boot', 30000)
 
-		if system.get_os() == 'win' then
-			vim.fn.readfile('C:/Users/runneradmin/AppData/Local/nvim-data/lsp.log')
-		end
 		assert.is_not_nil(jdtls, 'JDTLS should attach to Java buffer')
 		assert.is_not_nil(spring, 'Spring Boot should attach to Java buffer')
 	end)
