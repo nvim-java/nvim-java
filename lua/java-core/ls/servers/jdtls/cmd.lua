787523792f71816d049772d4ffc16ded6bac497f
Srinesh Nisala <srineshnisala@gmail.com>
2025-12-06 02:47:31 +0530

    fix: user config is not applied due to default config import in some modules (#443)

--- lua/java-core/ls/servers/jdtls/cmd.lua
+++ lua/java-core/ls/servers/jdtls/cmd.lua
@@ -1,7 +1,6 @@
 local List = require('java-core.utils.list')
 local path = require('java-core.utils.path')
 local Manager = require('pkgm.manager')
-local conf = require('java.config')
 local system = require('java-core.utils.system')
 local log = require('java-core.utils.log2')
 local err = require('java-core.utils.errors')
@@ -10,31 +9,29 @@ local lsp_utils = require('java-core.utils.lsp')
 
 local M = {}
 
-local jdtls_root = Manager:get_install_dir('jdtls', conf.jdtls.version)
-
 --- Returns a function that returns the command to start jdtls
----@param opts { use_lombok: boolean }
-function M.get_cmd(opts)
+---@param config java.Config
+function M.get_cmd(config)
 	---@param dispatchers? vim.lsp.rpc.Dispatchers
-	---@param config vim.lsp.ClientConfig
-	return function(dispatchers, config)
-		local cmd = M.get_jvm_args(opts):concat(M.get_jar_args())
+	---@param lsp_config vim.lsp.ClientConfig
+	return function(dispatchers, lsp_config)
+		local cmd = M.get_jvm_args(config):concat(M.get_jar_args(config))
 
 		-- NOTE: eventhough we are setting the PATH env var, due to a bug, it's not
 		-- working on Windows. So just lanching 'java' will result in executing the
 		-- system java. So as a workaround, we use the absolute path to java instead
 		-- So following check is not needed when we have auto_install set to true
 		-- @see https://github.com/neovim/neovim/issues/36818
-		if not conf.jdk.auto_install then
-			M.validate_java_version(config.cmd_env)
+		if not config.jdk.auto_install then
+			M.validate_java_version(config, lsp_config.cmd_env)
 		end
 
 		log.debug('Starting jdtls with cmd', cmd)
 
 		local result = vim.lsp.rpc.start(cmd, dispatchers, {
-			cwd = config.cmd_cwd,
-			env = config.cmd_env,
-			detached = config.detached,
+			cwd = lsp_config.cmd_cwd,
+			env = lsp_config.cmd_env,
+			detached = lsp_config.detached,
 		})
 
 		return result
@@ -42,9 +39,11 @@ function M.get_cmd(opts)
 end
 
 ---@private
----@param opts { use_lombok: boolean }
+---@param config java.Config
 ---@return java-core.List
-function M.get_jvm_args(opts)
+function M.get_jvm_args(config)
+	local use_lombok = config.lombok.enable
+	local jdtls_root = Manager:get_install_dir('jdtls', config.jdtls.version)
 	local jdtls_config = path.join(jdtls_root, system.get_config_suffix())
 
 	local java_exe = 'java'
@@ -52,8 +51,8 @@ function M.get_jvm_args(opts)
 	-- NOTE: eventhough we are setting the PATH env var, due to a bug, it's not
 	-- working on Windows. So we are using the absolute path to java instead
 	-- @see https://github.com/neovim/neovim/issues/36818
-	if conf.jdk.auto_install then
-		local jdk_root = Manager:get_install_dir('openjdk', conf.jdk.version)
+	if config.jdk.auto_install then
+		local jdk_root = Manager:get_install_dir('openjdk', config.jdk.version)
 		local java_home
 		if system.get_os() == 'mac' then
 			java_home = vim.fn.glob(path.join(jdk_root, 'jdk-*', 'Contents', 'Home'))
@@ -84,8 +83,8 @@ function M.get_jvm_args(opts)
 	})
 
 	-- Adding lombok
-	if opts.use_lombok then
-		local lombok_root = Manager:get_install_dir('lombok', conf.lombok.version)
+	if use_lombok then
+		local lombok_root = Manager:get_install_dir('lombok', config.lombok.version)
 		local lombok_path = vim.fn.glob(path.join(lombok_root, 'lombok*.jar'))
 		jvm_args:push('-javaagent:' .. lombok_path)
 	end
@@ -94,9 +93,11 @@ function M.get_jvm_args(opts)
 end
 
 ---@private
+---@param config java.Config
 ---@param cwd? string
 ---@return java-core.List
-function M.get_jar_args(cwd)
+function M.get_jar_args(config, cwd)
+	local jdtls_root = Manager:get_install_dir('jdtls', config.jdtls.version)
 	cwd = cwd or vim.fn.getcwd()
 
 	local launcher_reg = path.join(jdtls_root, 'plugins', 'org.eclipse.equinox.launcher_*.jar')
@@ -121,15 +122,16 @@ function M.get_jar_args(cwd)
 end
 
 ---@private
+---@param config java.Config
 ---@param env table
-function M.validate_java_version(env)
+function M.validate_java_version(config, env)
 	local curr_ver = M.get_java_major_version(env)
-	local exp_ver = java_version_map[conf.jdtls.version]
+	local exp_ver = java_version_map[config.jdtls.version]
 
 	if not (curr_ver >= exp_ver.to and curr_ver <= exp_ver.from) then
 		local msg = string.format(
 			'Java version mismatch: JDTLS %s requires Java %d <= java >= %d, but found Java %d',
-			conf.jdtls.version,
+			config.jdtls.version,
 			exp_ver.from,
 			exp_ver.to,
 			curr_ver
