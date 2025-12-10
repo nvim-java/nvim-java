5f047ed757cea1c3fb01e7023b3e1abc876ecb3a
Srinesh Nisala <srineshnisala@gmail.com>
2025-12-11 02:07:35 +0530

    chore: bump min nvim version to 0.11.5 (#456)

--- lua/java/checks/nvim-version.lua
+++ lua/java/checks/nvim-version.lua
@@ -7,13 +7,11 @@ function M:run(config)
 		return
 	end
 
-	if vim.fn.has('nvim-0.11') ~= 1 then
+	if not vim.version.ge(vim.version(), '0.11.5') then
 		local err = require('java-core.utils.errors')
 		err.throw([[
-				nvim-java is only tested on Neovim 0.11 or greater
-				Please upgrade to Neovim 0.11 or greater.
-				If you are sure it works on your version, disable the version check:
-				 checks = { nvim_version = false }'
+				nvim-java is only tested on Neovim 0.11.5 or greater
+				Please upgrade to Neovim 0.11.5 or greater.
 			]])
 	end
 end
